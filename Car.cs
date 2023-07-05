5aa8db41ffc5196596233b255c4630d9625d7d75
Максимова Анастаися <imbq@gmail.com>
2023-07-05 11:50:44 +0300

    Добавьте файлы проекта.

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿// Car.cs
+using System;
+
+namespace NazarovaUlyana.Library
+{
+    public class Car : IComparable
+    {
+        public string Owner { get; set; }
+        public string Number { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine($"Car Owner: {Owner}, Number: {Number}");
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Car otherCar = obj as Car;
+            if (otherCar != null)
+                return this.Owner.CompareTo(otherCar.Owner);
+            else
+                throw new ArgumentException("Object is not a Car");
+        }
+    }
+}
---
+++ Garage.cs
@@ -0,0 +1,29 @@
+﻿// Garage.cs
+using System;
+using System.Collections.Generic;
+
+namespace NazarovaUlyana.Library
+{
+    public class Garage
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Car> Cars { get; set; }
+
+        public Garage()
+        {
+            Cars = new List<Car>();
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine($"Garage: {Name}, Address: {Address}");
+            foreach (Car car in Cars)
+            {
+                car.Show();
+            }
+        }
+    }
+}
+
+
